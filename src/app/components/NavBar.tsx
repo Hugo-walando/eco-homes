bae410d59a9ea2db050f24c3659da7a7b28b96c0
Hugo-walando <hugowalando@gmail.com>
2024-10-11 17:42:23 +0200

    added pages & base component (navbar & footer)

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const navLinks = [
+  { name: 'Home', href: '/home' },
+  { name: 'About', href: '/about' },
+  { name: 'Services', href: '/services' },
+  { name: 'Projects', href: '/projects' },
+  { name: 'FAQ', href: '/faq' },
+];
+
+export default function NavBar() {
+  const pathname = usePathname();
+  return (
+    <>
+      <nav>
+        <ul>
+          {navLinks.map((link) => {
+            const isActive = pathname.startsWith(link.href);
+            return (
+              <li key={link.href}>
+                <Link
+                  className={isActive ? 'text-red-500' : 'text-black'}
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
+}
