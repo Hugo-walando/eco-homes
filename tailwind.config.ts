afb786e48a412468f0e3e03e888eb73a48411275
Hugo-walando <hugowalando@gmail.com>
2024-10-11 23:35:54 +0200

    added font file & custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        col_gray_1: '#605C53',
+        col_gray_2: '#909090',
+        col_white_1: '#F3F4ED',
+        col_white_2: '#FBF8F3',
+        col_primary_1: '#FF5912',
       },
     },
   },
