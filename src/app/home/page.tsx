e5f96b11754a111d9d20c29e797ef511ccee5d6e
Hugo-walando <hugowalando@gmail.com>
2024-10-21 15:50:24 +0200

    minor fix on the first section height

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
 
   return (
     <>
-      <section className='w-full min-h-screen h-screen'>
+      <section className='w-full h-dvh'>
         <Image
           src={Cabin}
           alt='Cabin'
