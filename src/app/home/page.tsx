e49284abccaef0d25301fa287daecfba1ba84e5d
Hugo-walando <hugowalando@gmail.com>
2024-10-25 14:02:41 +0200

    fix: changed the main image height

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
         <Image
           src={Cabin}
           alt='Cabin'
-          className='w-full h-full object-cover absolute'
+          className='w-full h-dvh object-cover absolute'
           priority
         />
         <div className='z-0 absolute inset-0 bg-black bg-opacity-20'></div>
